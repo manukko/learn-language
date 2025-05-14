e2cd1336f38da4dc5f06e062e2b1d695a9bd30dd
manukko <45536493+manukko@users.noreply.github.com>
2025-05-14 12:53:57 +0200

    change db and import from csvs to allow multiple translations

--- src/db/models.py
+++ src/db/models.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, create_engine, text, Index, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, create_engine, text, Index
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base, deferred
 import env
 from sqlalchemy.dialects import postgresql
@@ -32,15 +32,21 @@ class User(Base):
 class Word(Base):
     __tablename__ = "words"
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    name = Column(String, nullable=False)
-    translation = Column(String, nullable=False)
+    source_word = Column(String, nullable=False)
     language = Column(String, nullable=False, index=True)
     frequency = Column(Integer, nullable=False)
+    translations = relationship("WordTranslation", back_populates="word", cascade="all")
     __table_args__ = (
         Index('ix_unique_language_frequency', 'language', 'frequency', unique=True),
-        Index('ix_unique_language_name', 'language', 'name', unique=True),
-        UniqueConstraint('language', 'name', name='unique_language_name'),
+        Index('ix_unique_language_word', 'language', 'source_word', unique=True)
     )
+
+class WordTranslation(Base):
+    __tablename__ = "word_translations"
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    word_id = Column(Integer, ForeignKey("words.id", ondelete="CASCADE"))
+    translation = Column(String, nullable=False)
+    word = relationship("Word", back_populates="translations")
 
 class Game(Base):
     __tablename__ = "games"
@@ -79,23 +85,32 @@ def init_db():
     import_csvs_to_db()
     print("Database is ready.")
 
-def import_csvs_to_db(session=SessionLocal()):
+def import_csvs_to_db(db=SessionLocal()):
         for language in SUPPORTED_LANGUAGES:
-            result = session.query(Word).filter(Word.language == language).limit(1).first()
+            result = db.query(Word).filter(Word.language == language).limit(1).first()
             if not result:
                 print(f"No words present in Word table for language {language}: importing words from csvs...")
                 csv_file = f"{os.path.dirname(os.path.abspath(__file__))}/csv/{language}.csv"
                 with open(csv_file, mode='r', encoding='utf-8') as file:
                     csv_reader = csv.DictReader(file)
                     for row in csv_reader:
-                        word_translation = Word(
-                            name=row['Word'],
+                        source_word = row['Word']
+                        word = db.query(Word).filter(Word.source_word == source_word).first()
+                        if word is None:
+                            word = Word(
+                                source_word=source_word,
+                                language=language,
+                                frequency=int(row['Frequency'])
+                            )
+                        db.add(word)
+                        db.commit()
+                        db.refresh(word)
+                        translation = WordTranslation(
                             translation=row['Translation'],
-                            language=language,
-                            frequency=int(row['Frequency'])
+                            word_id=word.id
                         )
-                        session.add(word_translation)
-                session.commit()
+                        db.add(translation)
+                        db.commit()
                 print(f"Data imported successfully for language: {language}!")
 
 if __name__=="__main__":
