ef80c8f9b0f0fe4ea0930767c3acae0a0663624e
manukko <45536493+manukko@users.noreply.github.com>
2025-05-21 13:00:39 +0200

    update game models

--- src/schemas/games.py
+++ src/schemas/games.py
@@ -29,13 +29,15 @@ class GameDetailOutputModel(GameOutputModel):
 
     Attributes:
         n_correct_answers (int): The number of correct answers given by the player.
-        n_remaining_words_to_guess (int): The number of words left to guess in the game.
-        remaining_words_to_guess (List[str]): A list of words that have not yet been guessed.
+        n_remaining_words_to_guess (int): The total number of words left to guess in the game.
+        from_target_language (List[str]): The list of remaining words you have to translate from target language to yours.
+        from_your_language (List[str]): A list of remaining words you have to translate from your language to target.
         game_score_percentage (float | None): The player's score as a percentage, or None if not applicable.
     """
     n_correct_answers: int
     n_remaining_words_to_guess: int
-    remaining_words_to_guess: List[str]
+    from_target_language: List[str]
+    from_your_language: List[str]
     game_score_percentage: float | None
 
 class GameCreateInputModel(BaseModel):
@@ -47,11 +49,14 @@ class GameCreateInputModel(BaseModel):
         n_vocabulary (int): The number of vocabulary words available for the game.
         n_words_to_guess (int, optional): The number of words the player needs to guess. Defaults to 10.
         type (str, optional): The type of game to create. Defaults to "random".
+        translate_from_your_language_percentage (str, optional): percentage of words to translate from your language to target language; \
+            100 - translate_to_your_language_percentage is the percentage of words to translate from target language to your language instead.
     """
     language: str
     n_vocabulary: int
     n_words_to_guess: int = 10
     type: str = "random"
+    translate_from_your_language_percentage: int = Field(default=0, ge=0, le=100)
 
 class AnswerInputModel(BaseModel):
     """
