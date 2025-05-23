c2221f36421fa1af7b77bd47d0e51350a65de5fe
manukko <45536493+manukko@users.noreply.github.com>
2025-05-23 02:52:01 +0200

    Update test_user.py

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -3,14 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from src import version
 from src.db.models import User
 
-def test_create_user(client, postgres_engine):
-    username = "manukko2"
+def test_create_user_twice(client, postgres_engine):
+    username = "prodigioso"
     password = "r1S0tt0N&"
-    email = "pipino@language.com"
+    email = "prodigioso@pipino.com"
 
     body = {"username":username,"password":password, "email": email}
     response: JSONResponse = client.post(f"/api/{version}/users/register", json=body)
-    print(response.json())
     assert response.status_code == 200
 
     with sessionmaker(bind=postgres_engine)() as db:
@@ -19,3 +18,16 @@ def test_create_user(client, postgres_engine):
         assert created_user is not None
         assert created_user.username == username
         assert created_user.email == email
+
+    body = {"username":username,"password":password, "email": email}
+    response: JSONResponse = client.post(f"/api/{version}/users/register", json=body)
+    assert response.status_code == 409
+
+def test_create_user_password_invalid(client):
+    username = "pirlottero"
+    password = "easy"
+    email = "pirlottero@pirlero.com"
+
+    body = {"username":username,"password":password, "email": email}
+    response: JSONResponse = client.post(f"/api/{version}/users/register", json=body)
+    assert response.status_code == 403
