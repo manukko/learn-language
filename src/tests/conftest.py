f0fbc64d78a698d123a34ec511c6c09bcb49c432
manukko <45536493+manukko@users.noreply.github.com>
2025-05-23 02:35:04 +0200

    Create conftest.py

---
+++ src/tests/conftest.py
@@ -0,0 +1,32 @@
+from fastapi.testclient import TestClient
+from testcontainers.postgres import PostgresContainer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from src import app
+import pytest
+from src.db.models import Base
+from src.services.auth import get_db_session
+
+@pytest.fixture(scope="session")
+def postgres_engine():
+    with PostgresContainer("postgres:15") as postgres:
+        engine = create_engine(postgres.get_connection_url())
+        Base.metadata.create_all(engine)
+        yield engine  # teardown happens when container closes
+
+@pytest.fixture
+def override_get_db(postgres_engine):
+    TestingSessionLocal = sessionmaker(bind=postgres_engine)
+
+    def _override():
+        db = TestingSessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
+
+    app.dependency_overrides[get_db_session] = _override
+
+@pytest.fixture
+def client(override_get_db):
+    return TestClient(app)
