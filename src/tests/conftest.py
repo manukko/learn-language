1d52a9f2b3a26360040516375f819714734b1d3c
manukko <45536493+manukko@users.noreply.github.com>
2025-05-27 14:43:50 +0200

    fix tests configuration
    - avoid creating a container for each test
    - do not recreate and drop all metadata but truncate tables only

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,21 +1,29 @@
 from fastapi.testclient import TestClient
 from testcontainers.postgres import PostgresContainer
-from sqlalchemy import create_engine
+from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import sessionmaker
 from src import app
 import pytest
 from src.db.models import Base
 from src.services.auth import get_db_session
 
-@pytest.fixture(scope="function")
-def postgres_engine():
-    with PostgresContainer("postgres:15") as postgres:
-        engine = create_engine(postgres.get_connection_url())
-        Base.metadata.create_all(engine)
-        yield engine
+@pytest.fixture(scope="session")
+def postgres_container():
+    container = PostgresContainer("postgres:15")
+    container.start()
+    yield container
+    container.stop()
+
+
+@pytest.fixture(scope="session")
+def postgres_engine(postgres_container: PostgresContainer):
+    engine: Engine = create_engine(postgres_container.get_connection_url())
+    Base.metadata.create_all(bind=engine)
+    yield engine
+    Base.metadata.drop_all(bind=engine)
 
 @pytest.fixture(scope="function")
-def override_get_db(postgres_engine):
+def override_get_db(postgres_engine: Engine):
     TestingSessionLocal = sessionmaker(bind=postgres_engine)
 
     def _override():
@@ -24,11 +32,18 @@ def override_get_db(postgres_engine):
             yield db
         finally:
             db.close()
-
+    
     app.dependency_overrides[get_db_session] = _override
     yield
-    Base.metadata.drop_all(postgres_engine)
+    # Truncate all tables after each test (keeping schema)
+    with postgres_engine.connect() as connection:
+        trans = connection.begin()
+        for table in reversed(Base.metadata.sorted_tables):
+            connection.execute(table.delete())
+        trans.commit()
+
     app.dependency_overrides.clear()
+
 
 @pytest.fixture(scope="function")
 def client(override_get_db):
